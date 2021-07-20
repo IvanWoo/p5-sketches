a761199955e205914d5bfb73e8d193b8a284a5d2
Yifan Wu <tyivanwu@gmail.com>
2021-07-20 00:07:54 -0400

    chore: update yifan210719

--- src/sketches/yifan210719.ts
+++ src/sketches/yifan210719.ts
@@ -11,20 +11,27 @@ export const sketch = (p: p5) => {
     let speed = 0.001;
     let magicKnob = 4.2;
     magicKnob = 8.21;
-    magicKnob = 3;
+    // magicKnob = 3;
+    const initIsBlack = true;
+    const lightGrey = "#d3d3d3";
 
     p.setup = () => {
         p.createCanvas(width, height);
-        p.background("white");
+        p.background(lightGrey);
         // p.noLoop();
         p.frameRate(30);
     };
 
     p.draw = () => {
         p.translate(centerX, centerY);
-        p.background("white");
+        p.background(lightGrey);
         p.rotate(p.millis() * speed);
-        drawCircle(0, 0, constrainR * 2, true);
+
+        // the static canvas
+        p.fill(initIsBlack ? "black" : "white");
+        p.circle(0, 0, constrainR * 2 * 1.01);
+
+        drawCircle(0, 0, constrainR * 2, initIsBlack);
     };
 
     const clamp = (min: number, max: number, val: number) => {
@@ -35,7 +42,7 @@ export const sketch = (p: p5) => {
         p.fill(isBlack ? "black" : "white");
         p.noStroke();
         p.circle(x, y, d);
-        if (d > 10) {
+        if (d > 30) {
             let leftBoundary = Math.abs(x - d / 2);
             let rightBoundary = Math.abs(x + d / 2);
             let newX = clamp(-d, d, x + magicKnob * p.sin(x + centerX));
